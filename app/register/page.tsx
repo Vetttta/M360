17fb885d34243bf875b3ad7625c4301f7530666c
Vetta <l93434292@outlook.com>
2026-08-06 16:46:49 +0300

    Register page notification fix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -8,6 +8,9 @@ export default function Page() {
           <p className="font-semibold text-[21px] tracking-[-0.035em]">M360</p>
         </div>
       </div>
+        <div className="bg-bg p-[20px]">
+        <p className="text-[17px] w-bgL mx-auto text-light-gray-bg leading-[1.35]">Discover the latest improvements and new APIs in Apple Pay. You'll discover enhancements to the Apple Pay eCommerce experience Discover the latest improvements and new APIs in Apple Pay. You'll discover enhancements to the Apple Pay eCommerce experience</p>
+      </div>
       <RegisterForm />
     </div>
   )

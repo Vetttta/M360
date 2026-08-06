da1d3e12b16d901e7703d718b2fd1d80fd416556
Vetta <l93434292@outlook.com>
2026-08-06 16:45:01 +0300

    Register page header fix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,13 @@ import RegisterForm from "@/app/ui/register-form";
 
 export default function Page() {
   return (
-    <RegisterForm />
+    <div className="">
+      <div className="bg-[#151516]">
+        <div className="h-[52px] w-full max-w-[1024px] px-[22px] mx-auto flex items-center justify-between">
+          <p className="font-semibold text-[21px] tracking-[-0.035em]">M360</p>
+        </div>
+      </div>
+      <RegisterForm />
+    </div>
   )
 };
